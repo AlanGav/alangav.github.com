ffd93962dbd042bc10a66ee21fbbeff7c784d755
Alan Garcia Vega <alansoloprogramacion@gmail.com>
2013-11-20 23:22:55 -0600

    Update Web Borbor

--- borbor/js/efectos.js
+++ borbor/js/efectos.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 	$("#progressbar").css('opacity','0')
 	$("#porcentaje").css('opacity','0')
 	$("#spanish").click(function(){
-		$("#efecto_desliz").css('margin-left','75px')
+	$("#efecto_desliz").css('margin-left','75px')
+	$("#progressbar").css('opacity','1')
+		$("#porcentaje").css('opacity','1')
 		loaded = 0
 	var load = function(){
 			loaded+=10;
@@ -14,6 +16,10 @@ $(document).ready(function(){
 			$("#progressbar > div").css('background','#000')
 			dummyload();	
 			}
+			else{
+				document.location.href="inicio.html"
+			}
+
 		};
 		var dummyload = setInterval(function(){
 			load();
@@ -39,6 +45,6 @@ $(document).ready(function(){
 		var dummyload = setInterval(function(){
 			load();
 		},250)
-	
+		
 	})
 })
