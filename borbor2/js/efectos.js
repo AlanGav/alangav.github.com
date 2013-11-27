09a3393ca3e62e56d0f78f77f971065aa6e8d022
Alan Garcia Vega <alansoloprogramacion@gmail.com>
2013-11-26 23:05:16 -0600

    Slider Borbor

--- borbor2/js/efectos.js
+++ borbor2/js/efectos.js
@@ -1,21 +1,5 @@
 $(document).ready(function(){
-	ancho = $("nav ul li").width()-50
-	$("nav ul").append("<div id=efecto></div>")
-	$("#efecto").css({
-		height:'2px',
-		background:'#fff',
-		width:ancho,
-		position:'absolute'
-	})
-	$("#efecto").css("left",$("nav ul li").position().left+25+"px")
-	$("#efecto").css('width',$("nav ul li:first").width()-60+"px")
-	$("nav ul li a").click(function(){
-		//$("#efecto").css("left",$(this).parent().position().left+"px")
-		$("#efecto").css('width',$(this).parent().width()-60+"px")
-		$("#efecto").animate({
-			left:$(this).parent().position().left+25+"px"
-		})
-	})
+
 
 	$("#progressbar").css('opacity','0')
 	$("#porcentaje").css('opacity','0')
