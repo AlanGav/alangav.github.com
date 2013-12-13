77d1828b1a972fac27cb8650a4ab207770fe88b6
Alan Garcia Vega <alansoloprogramacion@gmail.com>
2013-12-13 12:53:21 -0600

    commit prueba

--- borbor2/js/menu.js
+++ borbor2/js/menu.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	$("#nosotros_flotante").hide()
+	$("#nosotros_flotante").hide(0)
+	$("#proyectos_flotante").hide(0)
 	$("#sliderDos").hide()
 	ancho = $("nav ul li:first").width()-50
 	$("nav ul").append("<div id=efecto></div>")
@@ -9,27 +10,30 @@ $(document).ready(function(){
 		width:ancho,
 		position:'absolute'
 	})
-	$("#efecto").css("left",$("nav ul li").position().left+25+"px")
-	$("#efecto").css('width',$("nav ul li:first").width()-60+"px")
+	$("#efecto").css("left",$("nav ul li a:first").position().left-5+"px")
+	$("#efecto").css('width',$("nav ul li a:first").width()+8+"px")
 	$("#efecto").hide(0,function(){
-		$(this).show(500)
+		$(this).show(800)
 	})
 
 	$("nav ul li a").on('mouseover',function(){
-	$("#efecto").css('width',$(this).parent().width()-60+"px")
+	$("#efecto").css('width',$(this).width()+8+"px")
 		$("#efecto").animate({
-			left:$(this).parent().position().left+25+"px"
+			left:$(this).position().left+"px"
 		})
 	})
-	
-
 	//Situar flotantes
-	
-
 	slider = $(".slider").position()
 	$("#nosotros_flotante").css({
 		"left":slider.left+43,
 		"width":'420px',
+		"height":'330px',
+		"position":'absolute',
+		"z-index":'100'
+	})
+	$("#proyectos_flotante").css({
+		"left":slider.left+43,
+		"width":'440px',
 		"height":'310px',
 		"position":'absolute',
 		"z-index":'100'
@@ -54,11 +58,13 @@ $(document).ready(function(){
 		
 
 	})
+	//Al dar click en inicio se cierra cualquier flotante abierto
 	$("#inicio").on('click',function () {
 		$(".flotante").fadeOut(500)
 	})
-	
+	//Aparecer el flotante de nosotros y cambiar el texto respecto al click
 	$("#nosotros").on('click',function(){
+		$("#proyectos_flotante").hide(0)
 		$("#nosotros_flotante").fadeIn(500)
 		$("#info_flotante").load('nosotros.html #qs',function(){
 			$(this).hide(0,function(){
@@ -66,9 +72,24 @@ $(document).ready(function(){
 			})
 		})
 	})
-	//alert(window.innerHeight+" ancho: "+window.innerWidth)
+	//Aparecer el flotante proyectos
+	$("#proyectos").on('click',function(){
+		$(".flotante").hide(0,function(){
+			$("#proyectos_flotante").fadeIn(500)	
+		})
+		
+		
+	})
+	//Aparecer el flotante clientes
+	$("#clientes").on('click',function(){
+		$(".flotante").hide(0,function(){
+			$("#clientes_flotante").fadeIn(500)	
+		})
+		
+		
+	})
 	//Cargar slider aparte (pantalla negra)
-
+	
 	$("#nosotrosSlider").on('click',function(){			
 			
 		$("#sliderDos").fadeIn()
@@ -81,7 +102,7 @@ $(document).ready(function(){
 				top:'0px',
 				background:'#000',
 				width:window.innerWidth+"px",
-				height:window.innerHeight+10+"px",
+				height:$("body").height()+"px",
 				"z-index":'200',
 				"margin-top":"3em"
 			})
